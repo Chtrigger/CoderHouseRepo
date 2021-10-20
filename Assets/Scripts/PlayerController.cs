21a601ae711a041e8e423936eed87ad2ebc55668
Gastón Benito <92412335+Chtrigger@users.noreply.github.com>
2021-10-20 19:37:56 -0300

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("En el frame inicial:");
+        Debug.Log(transform.position);
+        transform.localScale = new Vector3(3, 3, 3);
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("Por cada frame..");
+        transform.position += new Vector3(1,0,0);
+    }
+}
